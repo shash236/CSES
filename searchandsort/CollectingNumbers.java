eeb8d2910c2dde8a93015dfc8b1e6a26594220ae
shash236 <23shashwatik1999@gmail.com>
2024-03-25 07:49:59 +0530

    25th March

---
+++ searchandsort/CollectingNumbers.java
@@ -0,0 +1,84 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CollectingNumbers {
+
+    public static void main(String[] args) {
+        FastReader scanner = new FastReader();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        out.println(solve(arr, n));
+        
+        out.flush();
+        out.close();
+    }
+    
+    private static int solve(int[] arr, int n) {
+        int[] positions = new int[n+1];
+        int rounds = 1;
+        for(int i = 0; i < n; i++) {
+            positions[arr[i]] = i+1;
+        }
+        for(int i = 1; i <= n; i++) {
+            if(positions[i] < positions[i-1]){
+                rounds++;
+            }
+        }
+        return rounds;
+    }
+
+    static class FastReader {
+ 
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
---
+++ searchandsort/MissingCoinSum.java
@@ -0,0 +1,83 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class MissingCoinSum {
+    
+    public static void main(String[] args) {
+        FastReader scanner = new FastReader();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        out.println(solve(arr, n));
+        
+        out.flush();
+        out.close();
+    }
+
+    private static long solve(int[] arr, int n) {
+        Arrays.sort(arr);
+        long sum = 0;
+        for(int i = 0; i < n; i++) {
+            if(sum < arr[i] - 1){
+                return sum + 1;
+            }
+            sum += arr[i];
+        }
+        return sum + 1;
+    }
+
+     static class FastReader {
+ 
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
