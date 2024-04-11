efdaebe53996d1978dde95492a5192365279d395
shash236 <23shashwatik1999@gmail.com>
2024-04-11 14:41:28 +0530

    10th April

--- dynamicprogramming/MinimizingCoins.java
+++ dynamicprogramming/MinimizingCoins.java
@@ -1,3 +1,87 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
 public class MinimizingCoins {
+    public static void main(String[] args) {
+        FastReader scanner = new FastReader();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        out.println(solve(x, n, arr));
+        out.flush();
+        out.close();
+    }
     
+    private static int solve(int x, int n, int[] coins) {
+        int[] dp = new int[x+1];
+        for(int val = 1; val <= x; val++) {
+            int minCoins = Integer.MAX_VALUE;
+            for(int i = 0; i < n; i++){
+                if(val - coins[i] >= 0 && dp[val - coins[i]] != -1){
+                    minCoins = Math.min(minCoins,1 + dp[val - coins[i]]);
+                }
+            }
+            if(minCoins != Integer.MAX_VALUE) {
+                dp[val] = minCoins;
+            }else {
+                dp[val] = -1;
+            }
+        }
+        return dp[x];
+    }
+
+    static class FastReader {
+ 
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
 }
--- searchandsort/ReadingBooks.java
+++ searchandsort/ReadingBooks.java
@@ -4,47 +4,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
-public class RestaurantCustomers {
-    
+public class ReadingBooks {
     public static void main(String[] args) {
         FastReader scanner = new FastReader();
         PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
 
         int n = scanner.nextInt();
-        int[][] arr = new int[n][2];
+        int[] arr = new int[n];
         for(int i = 0; i < n; i++) {
-            arr[i][0] = scanner.nextInt();
-            arr[i][1] = scanner.nextInt();
+            arr[i] = scanner.nextInt();
         }
-        out.println(solve(arr, n));
+        out.println(solve(n, arr));
         out.flush();
         out.close();
     }
 
-    private static int solve(int[][] arr, int n){
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(int i = 0; i< n; i++) {
-            if(map.get(arr[i][0])==null){
-                map.put(arr[i][0],1);
-            }else{
-                map.put(arr[i][0],map.get(arr[i][0])+1);
-            }
-
-            if(map.get(arr[i][1])==null){
-                map.put(arr[i][1],-1);
-            }else{
-                map.put(arr[i][1],map.get(arr[i][1])-1);
-            }
+    private static long solve(int n, int[] arr) {
+        long sum = 0;
+        long maxVal = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            maxVal = Math.multiplyExact(maxVal, arr[i]);
         }
-        int curr = 0;
-        int maxval = 0;
-        for(int pos : map.keySet()){
-            curr += map.get(pos);
-            maxval = Math.max(curr,maxval);
-        }
-        return maxval;
+        return Math.max(sum,2*maxVal);
     }
 
     static class FastReader {
