0fbb26724d300b7cbeda2c4f9901937ad77dd3ff
shash236 <23shashwatik1999@gmail.com>
2024-05-02 10:19:38 +0530

    2nd May

---
+++ searchandsort/SubarraySumsI.java
@@ -0,0 +1,91 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class SubarraySumsI {
+    public static void main(String[] args) {
+        FastReader scanner = new FastReader();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        out.println(solve(n, x, arr));
+        out.flush();
+        out.close();
+    }
+
+    private static int solve(int n, int x, int[] arr) {
+        Set<Long> set = new HashSet<>();
+        long prefixSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+            set.add(prefixSum);
+        }
+
+        int validSubArrays = 0;
+        prefixSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(set.contains(prefixSum + x)){
+                validSubArrays++;
+            }
+            prefixSum += arr[i];
+        }
+
+        return validSubArrays;
+    }
+
+    static class FastReader {
+ 
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
